32607d7c1fc4092d1437a603f1d3383c9fc1c7e6
Jantu Deb <63020886+JantuDeb@users.noreply.github.com>
2023-12-18 01:07:34 +0530

    nvm fix

--- api-server-configs/setup_script.sh
+++ api-server-configs/setup_script.sh
@@ -157,7 +157,10 @@ install_dependencies() {
 
     # Install Node.js and PM2
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-    source ~/.bashrc
+
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     nvm install --lts
     npm install pm2@latest -g
 }
